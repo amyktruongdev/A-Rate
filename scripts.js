1e9b48838c6ce22d95e8b6f7e79d259b52a66e01
Amy Truong <amyktruongdev@gmail.com>
2024-04-07 22:09:06 -0700

    testing user input data

--- scripts.js
+++ scripts.js
@@ -1,28 +1,3 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- * 
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your 
- *    browser and make sure you can see that change. 
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- * 
- */
-
 
 const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
 const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
@@ -30,15 +5,18 @@ const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/
 const FAIRY_TAIL = "images/fairytail.png";
 
 // This is an array of strings (TV show titles)
-let titles = [
+var title = [];
+var score = [];
+var imageURL = [];
+
+
+
+/*
     "Seven Deadly Sins",
     "That Time I Got Reincarnated as a Slime",
-    "East Los High", "Surgeon Elise", "Samurai Champloo", "Fairy Tail"
-];
+    "East Los High", "Surgeon Elise", "Samurai Champloo", "Fairy Tail"*/
 // Your final submission should have much more data than this, and 
 // you should use more than just an array of strings to store it all.
-
-
 // This function adds cards the page to display the data in the array
 function showCards() {
     const cardContainer = document.getElementById("card-container");
@@ -52,7 +30,8 @@ function showCards() {
         // This part of the code doesn't scale very well! After you add your
         // own data, you'll need to do something totally different here.
         let imageURL = "";
-        if (i == 0) {
+        
+        /*if (i == 0) {
             imageURL = FRESH_PRINCE_URL;
         } else if (i == 1) {
             imageURL = CURB_POSTER_URL;
@@ -67,7 +46,8 @@ function showCards() {
         }
         else if(i ==5){
             imageURL = FAIRY_TAIL;
-        }
+        }*/
+
 
         const nextCard = templateCard.cloneNode(true); // Copy the template card
         editCardContent(nextCard, title, imageURL); // Edit title and image
@@ -108,3 +88,29 @@ function removeLastCard() {
     }
     showCards(); // Call showCards again to refresh
 }
+
+/**
+ * Data Catalog Project Starter Code - SEA Stage 2
+ *
+ * This file is where you should be doing most of your work. You should
+ * also make changes to the HTML and CSS files, but we want you to prioritize
+ * demonstrating your understanding of data structures, and you'll do that
+ * with the JavaScript code you write in this file.
+ * 
+ * The comments in this file are only to help you learn how the starter code
+ * works. The instructions for the project are in the README. That said, here
+ * are the three things you should do first to learn about the starter code:
+ * - 1 - Change something small in index.html or style.css, then reload your 
+ *    browser and make sure you can see that change. 
+ * - 2 - On your browser, right click anywhere on the page and select
+ *    "Inspect" to open the browser developer tools. Then, go to the "console"
+ *    tab in the new window that opened up. This console is where you will see
+ *    JavaScript errors and logs, which is extremely helpful for debugging.
+ *    (These instructions assume you're using Chrome, opening developer tools
+ *    may be different on other browsers. We suggest using Chrome.)
+ * - 3 - Add another string to the titles array a few lines down. Reload your
+ *    browser and observe what happens. You should see a fourth "card" appear
+ *    with the string you added to the array, but a broken image.
+ * 
+ */
+
