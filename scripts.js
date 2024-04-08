ca106a0a58580e5e938873fa5d3c437c8b6b1866
Amy Truong <amyktruongdev@gmail.com>
2024-04-08 13:33:06 -0700

    updating js

--- scripts.js
+++ scripts.js
@@ -7,6 +7,18 @@ function addAnime(){
     message.innerHTML = "data: " + title.value + " " + score.value + " " + myfile.value;
 }
 
+const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
+const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
+const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+const FAIRY_TAIL = "images/fairytail.png";
+
+// This is an array of strings (TV show titles)
+let titles = [
+    "Seven Deadly Sins",
+    "That Time I Got Reincarnated as a Slime",
+    "East Los High", "Surgeon Elise", "Samurai Champloo", "Fairy Tail"
+];
+
 function showCards() {
     const cardContainer = document.getElementById("card-container");
     cardContainer.innerHTML = "";
