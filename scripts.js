85a1104ab17c860a1fa192c015ad657a3efa93ba
Amy Truong <amyktruongdev@gmail.com>
2024-04-08 13:31:56 -0700

    understanding js

--- scripts.js
+++ scripts.js
@@ -7,5 +7,30 @@ function addAnime(){
     message.innerHTML = "data: " + title.value + " " + score.value + " " + myfile.value;
 }
 
+function showCards() {
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = "";
+    const templateCard = document.querySelector(".card");
 
+ const nextCard = templateCard.cloneNode(true); // Copy the template card
+        editCardContent(nextCard, title, imageURL); // Edit title and image
+        cardContainer.appendChild(nextCard); // Add new card to the container
+}
+function editCardContent(card, newTitle, newImageURL) {
+    card.style.display = "block";
+
+    const cardHeader = card.querySelector("h2");
+    cardHeader.textContent = newTitle;
+
+    const cardImage = card.querySelector("img");
+    cardImage.src = newImageURL;
+    cardImage.alt = newTitle + " Poster";
+
+    // You can use console.log to help you debug!
+    // View the output by right clicking on your website,
+    // select "Inspect", then click on the "Console" tab
+    console.log("new card:", newTitle, "- html: ", card);
+}
+
+document.addEventListener("DOMContentLoaded", showCards);
 
