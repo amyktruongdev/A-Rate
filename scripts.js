51f9644c4dfa04d58c31393271aced2deffe0d07
Amy Truong <amyktruongdev@gmail.com>
2024-04-09 01:11:44 -0700

    synopsis added and functional

--- scripts.js
+++ scripts.js
@@ -3,63 +3,75 @@
 const anime1={
     titles: "Seven Deadly Sins",
     imageURL: "https://cdn.myanimelist.net/images/anime/8/65409.jpg",
-    ratings: 9.45
+    ratings: 9.45,
+    synopsis: "Seven Deadly Sins is one of the animes I can watch over and over again. I fall more in love with it each time I watch it. It's not a 10 because the fight scenes were lacking."
 }
 const anime2={
     titles: "That Time I Got Reincarnated as a Slime",
     imageURL: "https://cdn.myanimelist.net/images/anime/1069/123309.jpg",
-    ratings: 7.50
+    ratings: 7.50,
+    synopsis: "I gave this show a 7.50 even though I like the world building the show provides. It's just progressing too slow for me to enjoy."
 }
 const anime3={
     titles: "Neon Genesis Evangelion",
     imageURL: "https://cdn.myanimelist.net/images/anime/1314/108941.jpg",
-    ratings: 8.35
+    ratings: 8.35,
+    synopsis: "This is a really nostalgic anime for a lot of people. It's confusing to understand this show, but I really appreciate the art style behind Evangelion."
 }
 const anime4={
     titles: "Surgeon Elise",
     imageURL: "https://cdn.myanimelist.net/images/anime/1449/140344.jpg",
-    ratings: 9.30
+    ratings: 9.30,
+    synopsis: "Surgeon Elise has an extremely smart female protagonist who battles between becoming an empress of a kingdom or becoming a surgeon."
 }
 const anime5={
     titles: "Samurai Champloo",
     imageURL: "https://cdn.myanimelist.net/images/anime/1375/121599.jpg",
-    ratings: 8.30
+    ratings: 8.30,
+    synopsis: "Really cool anime with a sick art style. I heard the soundtrack for this show playing on a Hawaiian radio station."
 }
 const anime6={
     titles: "Fairy Tail",
     imageURL: "images/fairytail.png",
-    ratings: 10
+    ratings: 10,
+    synopsis: "The first ever anime I've ever watched. It holds a special place in my heart because it taught me what the power of family and love does for a person. Fairy tail for life!!"
 }
 
 const anime7={
     titles: "Attack on Titan: The Final Season",
     imageURL: "https://cdn.myanimelist.net/images/anime/1279/131078.jpg",
-    ratings: 9.90
+    ratings: 9.90,
+    synopsis: "I watched this anime while it was coming out when I was in middle school. It took around 7 years for this final season to come out. It definitely went out with a bang."
 }
 const anime8={
     titles: "Parasyte: The Maxim",
     imageURL: "https://cdn.myanimelist.net/images/anime/3/73178.jpg",
-    ratings: 9.10
+    ratings: 9.10,
+    synopsis: "A guy's right hand becomes an alien and now he has to protect his world from man eating aliens. He learns that the aliens weren't all that bad. I love the anime's opening song."
 }
 const anime9={
     titles: "Solo Leveling",
     imageURL: "https://cdn.myanimelist.net/images/anime/1411/142391.jpg",
-    ratings: 10
+    ratings: 10,
+    synopsis: "Solo leveling was a manhwa that just recently became an anime. This is the most recent anime I finished watching. The animation and plot is the best I've seen in a long time even though it doesn't follow the manhwa."
 }
 const anime10={
     titles: "Violet Evergarden",
     imageURL: "https://cdn.myanimelist.net/images/anime/1795/95088.jpg",
-    ratings: 9.30
+    ratings: 9.65,
+    synopsis: "Violet Evergarden is beautifuly animated show where a girl who has only known fighting her entire life tries to find the meaning of what the phrase I love you."
 }
 const anime11={
     titles: "Demon Slayer",
     imageURL: "https://cdn.myanimelist.net/images/anime/1286/99889.jpg",
-    ratings: 9.50
+    ratings: 9.50,
+    synopsis: "Extremely unique art style with a main protagonist with an extremely kind heart. His sister became a demon and now he's trying to turn her back into a human."
 }
 const anime12={
     titles: "Jujutsu Kaisen",
     imageURL: "https://cdn.myanimelist.net/images/anime/1171/109222.jpg",
-    ratings: 7.20
+    ratings: 7.20,
+    synopsis: "This show was nominated as the best anime of the year. I don't really agree with it. The fight scenes are animated really well, but I didn't enjoy the plot as much as it was hyped up to be."
 }
 
 
@@ -67,32 +79,38 @@ const anime12={
 const anime13={
     titles: "Darling in the FranXX",
     imageURL: "https://cdn.myanimelist.net/images/anime/1614/90408.jpg",
-    ratings: 4.15
+    ratings: 4.15,
+    synopsis: "I really disliked this show. The anime left out so much detail such as who the characters were fighting and why were they fighting them. The manga was made after the anime to be better but it just got worse."
 }
 const anime14={
     titles: "Akame ga Kill!",
     imageURL: "https://cdn.myanimelist.net/images/anime/1429/95946.jpg",
-    ratings: 5.30
+    ratings: 5.30,
+    synopsis: "All of the characters meet a not so good end and it didn't contribute much to the plot afterwards. Akame is a cool character though."
 }
 const anime15={
     titles: "Sword Art Online",
     imageURL: "https://cdn.myanimelist.net/images/anime/11/39717.jpg",
-    ratings: 9.95
+    ratings: 10,
+    synopsis: "I can't explain how much I love this show. It even taught me what love was. A guy plays a VR game and can't log out. If you die in game you die in real life."
 }
 const anime16={
     titles: "The Future Diary",
     imageURL: "https://cdn.myanimelist.net/images/anime/13/33465.jpg",
-    ratings: 6
+    ratings: 6.05,
+    synopsis: "The characters get emails of actions their future selves have already gone through. With this, they play a survival game. I rated it a 6 because they dropped the ball with the ending."
 }
 const anime17={
     titles: "Blue Exorcist",
     imageURL: "https://cdn.myanimelist.net/images/anime/10/75195.jpg",
-    ratings: 9.55
+    ratings: 9.55,
+    synopsis: "Another one of my all time favorite animes. The son of satan doesn't want to be evil and finds friends who also have compassion for him too. He's now fighting against his dad haha."
 }
 const anime18={
     titles: "Nana",
     imageURL: "https://cdn.myanimelist.net/images/anime/2/11232.jpg",
-    ratings: 10
+    ratings: 10,
+    synopsis: "I never thought a story about two girls named Nana would make me cry so much. They're both girls who are trying to navigate adulthood. The anime is unfinished as the author went on hiatus."
 }
 
 
@@ -100,33 +118,38 @@ const anime18={
 const anime19={
     titles: "Hunter x Hunter",
     imageURL: "https://cdn.myanimelist.net/images/anime/1337/99013.jpg",
-    ratings: 9.60
+    ratings: 9.60,
+    synopsis: "A boy goes out on a quest to become a Hunter. He outwits his opponents and has a heart of gold. It's one of the best animes for a reason."
 }
 const anime20={
     titles: "Tower of God",
     imageURL: "https://cdn.myanimelist.net/images/anime/1702/106229.jpg",
-    ratings: 5.30
-    //didnt closely follow manhwa ruined the show for me because the plot in the manhwa was already good
+    ratings: 5.30,
+    synopsis:"The show didn't closely follow manhwa. That ruined the show for me because the plot in the manhwa was already top tier."
 }
 const anime21={
     titles: "Erased",
     imageURL: "https://cdn.myanimelist.net/images/anime/10/77957.jpg",
-    ratings: 6.34
+    ratings: 6.34,
+    synopsis: "A guy goes back in time as his younger self and is trying to stop the murder of this other young girl. Amazing concept, but the ending episodes really ruined the entire show."
 }
 const anime22={
     titles: "Black Bullet",
     imageURL: "https://cdn.myanimelist.net/images/anime/1292/94693.jpg",
-    ratings: 5.34
+    ratings: 5.34,
+    synopsis: "A lot of characters were only there for plot points and didn't have any of their own development. A lot of scenes were made for shock factor and nothing more."
 }
 const anime23={
     titles: "Assassination Classroom",
     imageURL: "https://cdn.myanimelist.net/images/anime/5/75639.jpg",
-    ratings: 9
+    ratings: 9.25,
+    synopsis: "A yellow alien looking squid becomes a teacher. All of his students are tasked with killing him because he would destory the world. I cried so much."
 }
 const anime24={
     titles: "School Babysitters",
     imageURL: "https://cdn.myanimelist.net/images/anime/8/89978.jpg",
-    ratings: 8.68
+    ratings: 8.68,
+    synopsis: "This anime hit real close to home. A boy and his baby brother lose his parents in a car accident and try to navigate life together. It's tough but at least they have each other."
 }
 const anime = [anime1,anime2,anime3,anime4,anime5,anime6,anime7,anime8,anime9,anime10,anime11,anime12,anime13,anime14,anime15,anime16,anime17,anime18,anime19,anime20,anime21,anime22,anime23,anime24];
 
@@ -140,6 +163,7 @@ function showCards() {
         const r=anime[i];
         let title =r.titles;
         let image=r.imageURL;
+        let quote=r.synopsis;
         let rating = r.ratings.toFixed(2); //toFixed func incase i want to rate a show as "7.00" without toFixedFunc it outputs 7 without the 0's.
 
         //******FIX IF HAVE TIME MAKE IT SO THAT ALL WHOLE NUMBERS DONT RETURN 2 DECIMAL PLACES*******
@@ -147,12 +171,12 @@ function showCards() {
             rating = r.ratings;
         }
  const nextCard = templateCard.cloneNode(true); // Copy the template card
-        editCardContent(nextCard, title, image,rating); //edits title and image
+        editCardContent(nextCard, title, image,rating,quote); //edits title and image
         cardContainer.appendChild(nextCard); // Add new card to the container
     }           
 }
 
-function editCardContent(card, newTitle, newImageURL,newRating) { //edits card content by grabbig the element of classes and id's and assigning them based on data values above
+function editCardContent(card, newTitle, newImageURL,newRating, newSummary) { //edits card content by grabbig the element of classes and id's and assigning them based on data values above
     card.style.display = "block";
 
     //note querySelector takes first element, use period "." for a class and "#" for an ID
@@ -161,6 +185,9 @@ function editCardContent(card, newTitle, newImageURL,newRating) { //edits card c
 
     const cardImage = card.querySelector("img");//anime pic 
     cardImage.src = newImageURL;
+
+    const cardSynposis = card.querySelector("#summary");
+    cardSynposis.textContent = newSummary;
 
     const cardRating = card.querySelector("#score"); //anime rating
     cardRating.textContent= newRating;
