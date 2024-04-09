67219021a638eb85bfd7918ca36712808e31c188
Amy Truong <amyktruongdev@gmail.com>
2024-04-08 18:27:31 -0700

    clear list function working

--- scripts.js
+++ scripts.js
@@ -1,110 +1,86 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- * 
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your 
- *    browser and make sure you can see that change. 
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- * 
- */
+//creating objects of anime (anime1,anime2,anime3,anime4,anime5,anime6)
 
+const anime1={
+    titles: "Seven Deadly Sins",
+    imageURL: "https://cdn.myanimelist.net/images/anime/8/65409.jpg",
+    ratings: 9.45
+}
+const anime2={
+    titles: "That Time I Got Reincarnated as a Slime",
+    imageURL: "https://cdn.myanimelist.net/images/anime/1069/123309.jpg",
+    ratings: 7.50
+}
+const anime3={
+    titles: "Neon Genesis Evangelion",
+    imageURL: "https://cdn.myanimelist.net/images/anime/1314/108941.jpg",
+    ratings: 8.35
+}
+const anime4={
+    titles: "Surgeon Elise",
+    imageURL: "https://cdn.myanimelist.net/images/anime/1449/140344.jpg",
+    ratings: 9.30
+}
+const anime5={
+    titles: "Samurai Champloo",
+    imageURL: "https://cdn.myanimelist.net/images/anime/1375/121599.jpg",
+    ratings: 9
+}
+const anime6={
+    titles: "Fairy Tail",
+    imageURL: "images/fairytail.png",
+    ratings: 9.88
+}
+const anime = [anime1,anime2,anime3,anime4,anime5,anime6];
 
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
-const FAIRY_TAIL = "images/fairytail.png";
-
-// This is an array of strings (TV show titles)
-let titles = [
-    "Seven Deadly Sins",
-    "That Time I Got Reincarnated as a Slime",
-    "East Los High", "Surgeon Elise", "Samurai Champloo", "Fairy Tail"
-];
-
-// Your final submission should have much more data than this, and 
-// you should use more than just an array of strings to store it all.
-
-
-// This function adds cards the page to display the data in the array
 function showCards() {
     const cardContainer = document.getElementById("card-container");
     cardContainer.innerHTML = "";
     const templateCard = document.querySelector(".card");
-    
-    for (let i = 0; i < titles.length; i++) {
-        let title = titles[i];
 
-        // This part of the code doesn't scale very well! After you add your
-        // own data, you'll need to do something totally different here.
-        let imageURL = "";
-        if (i == 0) {
-            imageURL = FRESH_PRINCE_URL;
-        } else if (i == 1) {
-            imageURL = CURB_POSTER_URL;
-        } else if (i == 2) {
-            imageURL = EAST_LOS_HIGH_POSTER_URL;
-        }
-        else if(i ==3){
-            imageURL = CURB_POSTER_URL;
-        }
-        else if(i ==4){
-            imageURL = FRESH_PRINCE_URL;
-        }
-        else if(i ==5){
-            imageURL = FAIRY_TAIL;
-        }
+    for (let i = 0; i < anime.length; i++) {
+        const r=anime[i];
+        let title =r.titles;
+        let image=r.imageURL;
+        let rating = r.ratings.toFixed(2); //toFixed func incase i want to rate a show as "7.00" without toFixedFunc it outputs 7 without the 0's.
 
-        const nextCard = templateCard.cloneNode(true); // Copy the template card
-        editCardContent(nextCard, title, imageURL); // Edit title and image
+ const nextCard = templateCard.cloneNode(true); // Copy the template card
+        editCardContent(nextCard, title, image,rating); //edits title and image
         cardContainer.appendChild(nextCard); // Add new card to the container
-    }
+    }           
 }
 
-function editCardContent(card, newTitle, newImageURL) {
+function editCardContent(card, newTitle, newImageURL,newRating) { //edits card content by grabbig the element of classes and id's and assigning them based on data values above
     card.style.display = "block";
 
-    const cardHeader = card.querySelector("h2");
+    //note querySelector takes first element, use period "." for a class and "#" for an ID
+    const cardHeader = card.querySelector("h2"); //anime title
     cardHeader.textContent = newTitle;
 
-    const cardImage = card.querySelector("img");
+    const cardImage = card.querySelector("img");//anime pic 
     cardImage.src = newImageURL;
-    cardImage.alt = newTitle + " Poster";
 
-    // You can use console.log to help you debug!
-    // View the output by right clicking on your website,
-    // select "Inspect", then click on the "Console" tab
+    const cardRating = card.querySelector("#score"); //anime rating
+    cardRating.textContent= newRating;
+
+    cardImage.alt = newTitle + " Poster"; //if image doesnt load
+
+//used console to help me figure out why images weren't displaying properly, didn't really want to change this one.
     console.log("new card:", newTitle, "- html: ", card);
 }
 
-// This calls the addCards() function when the page is first loaded
 document.addEventListener("DOMContentLoaded", showCards);
 
-function removeLastCard() {
+function removeAllCards() {
     var userChoice; //ok or no option
     //while loop that clears all anime titles after pressing Clear button and asks for user confirmation.
     if(confirm("Are you sure you want to clear your anime list?") ==true){
-        while(titles.length){
-            let i = titles.pop();
+        while(anime.length){
+            anime.pop();
         }
-        userChoice=alert("List is being cleared!");
+        userChoice=alert("List IS being cleared!");
     }
     else{
-        userChoice=alert("List not canceled!");
+        userChoice=alert("List is NOT being canceled!");
     }
     showCards(); // Call showCards again to refresh
 }
